62d9926e203cc3e92eb7dfb1c83db2ad65ec2194
Theodore Corrello-Reiser <59662884+scottycr@users.noreply.github.com>
2026-02-24 21:55:13 -0700

    Comment out Python and TS language servers since I am not using them at the moment

--- init.lua
+++ init.lua
@@ -678,7 +678,7 @@ require('lazy').setup({
       local servers = {
         clangd = {},
         -- gopls = {},
-        pyright = {},
+        -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -686,7 +686,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        ts_ls = {},
+        -- ts_ls = {},
         --
 
         lua_ls = {
